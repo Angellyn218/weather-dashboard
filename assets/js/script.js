62f745439ef3556250b8a61e1e35784cf0f04211
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-10 14:37:38 -0700

    Got weather api and used fetch to see data

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// variables needed:
+// current city
+// array of cities (objects)
+// elements on html page (search form, city-sect, days-sect)
+// link variables (api-key, api link, city)
+var city = "San Mateo";
+var APIKey = "f347914f811d823cd34cc8b05f4907bc";
+
+// What to do
+
+// fetch weather forcast of city using link use
+function getApi() {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+// input data into city sect
+// fetch 5 day forcast of city
+// input data into days sect
+
+// create button for a city
+// city data into array then local storage
+// 
+getApi();
